a4da0bb3b16f5fc2969617fe62622f2d9cd78ec7
Koby Fink <finkkoby@icloud.com>
2024-02-01 15:31:04 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+
+function superbowlWin(array) {
+    let foundAWin = array.find(function(item) {
+        return item.result === "W";
+    })
+    if (foundAWin === undefined) {
+        return undefined
+    } else {
+        return foundAWin.year
+    }
+}
